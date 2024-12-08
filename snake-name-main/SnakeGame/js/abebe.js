26e361825efe0431306f51929eb98bcd747c2370
habtamu <habtamufentw12@gmail.com>
2024-12-08 14:26:47 -0800

    first commit

---
+++ snake-name-main/SnakeGame/js/abebe.js
@@ -0,0 +1,45 @@
+// function add(){
+//     console.log("hi")
+// }
+// // function minus(x, y) {
+// //   return x+y;
+// // }
+// moudule.exports.add=add
+// // moudule.exports.m=minus
+
+
+const abe = require('./abebe')
+console.log(abe.x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ snake-name-main/SnakeGame/js/kebede.js
@@ -0,0 +1,21 @@
+// // // function sum(a,y) {
+// // //     console.log(a+y)
+    
+// // // }
+// // // module.exports.sum = sum
+// // let a=require("js/abebe.js")
+// // console.log(a.mul(2,2))
+// let aa=require("./abebe.js")
+// // let mm = required("./js/abebe.js");
+// // console.log(aa.a(2,3)) 
+// aa.add(2, 3);
+
+var x=5
+moudule.exports.x=x
+
+
+
+
+
+
+
